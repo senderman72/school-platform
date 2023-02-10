212de4eff06423d8da76ff0b6e77e86ea387f411
Erik Persson <perssonerik7@gmail.com>
2023-02-10 13:01:04 +0100

    integrated database with prisma

--- server/index.js
+++ server/index.js
@@ -1,12 +1,38 @@
-const express = require("express");
+import { PrismaClient } from "@prisma/client";
+import express from "express";
 const app = express();
 const port = process.env.PORT || 3001;
+
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/users/:id", (req, res) => {
-  res.send(req.params.id);
+app.get("/users/:id", async (req, res) => {
+  let id = parseInt(req.params.id);
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  res.json(user);
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      name: name,
+      email: email,
+      password: password,
+    },
+  });
+  res.json(user);
 });
 
 app.listen(port, () => {
---
+++ server/prisma/migrations/20230210113017_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `role` ENUM('TEACHER', 'USER') NOT NULL DEFAULT 'USER',
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
