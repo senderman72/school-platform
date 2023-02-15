8938d1bd312ff6aa0845a28ca13cf05275c4f7a5
Erik Persson <perssonerik7@gmail.com>
2023-02-15 16:56:25 +0100

    created more routes

--- server/index.js
+++ server/index.js
@@ -36,9 +36,33 @@ app.post("/users", async (req, res) => {
 });
 
 app.get("/assignments", async (req, res) => {
-  const { id } = req.body;
-  const assignments = await prisma.assignment.findMany();
-  res.json(assignments);
+  const assignment = await prisma.assignment.findMany();
+
+  res.json(assignment);
+});
+
+app.get("/assignments/:userId", async (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  if (user.role === "TEACHER") {
+    // If the user is a teacher, return all assignments
+    const assignments = await prisma.assignment.findMany();
+    return res.json(assignments);
+  } else {
+    // If the user is a student, return assignments they have submitted
+    const submissions = await prisma.submissions.findMany({
+      where: { userId: userId, submitted: true },
+      include: { assignment: true },
+    });
+    return res.json(submissions.map((submission) => submission.assignment));
+  }
 });
 
 app.post("/assignments", async (req, res) => {
@@ -54,6 +78,37 @@ app.post("/assignments", async (req, res) => {
     },
   });
   res.json(assignment);
+});
+
+app.post("/assignments/:assignmentId/submit", async (req, res) => {
+  const assignmentId = req.params.assignmentId;
+  const userId = req.body.userId;
+
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const assignment = await prisma.assignment.findUnique({
+    where: { id: assignmentId },
+  });
+
+  if (!assignment) {
+    return res.status(404).json({ message: "Assignment not found" });
+  }
+
+  const submission = await prisma.submissions.create({
+    data: {
+      submitted: true,
+      userId: user.id,
+      assignmentId: assignment.id,
+    },
+  });
+
+  res.json(submission);
 });
 
 app.listen(port, () => {
